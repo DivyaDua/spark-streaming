8bf17238dc81cacaefac29f06a7330c71b54e633
Divya Dua <divya.dua@Divyas-MacBook-Pro.local>
2020-02-28 15:31:12 +0530

    Added spark streaming sample test class

---
+++ build.sbt
@@ -0,0 +1,12 @@
+name := "TestProject"
+
+version := "0.1"
+
+scalaVersion := "2.12.8"
+
+libraryDependencies ++= Seq(
+  "org.apache.spark" %% "spark-core" % "2.4.4",
+  "org.apache.spark" %% "spark-sql" % "2.4.4",
+  "org.apache.logging.log4j" % "log4j-core" % "2.13.0"
+  //"org.apache.spark" %% "spark-streaming" % "2.4.4" % "provided"
+)
---
+++ src/main/scala/com/thoughtworks/TestClass.scala
@@ -0,0 +1,36 @@
+package com.thoughtworks
+
+import org.apache.log4j.{Level, Logger}
+import org.apache.spark.sql.{DataFrame, SparkSession}
+
+object TestClass {
+
+  def main(args: Array[String]): Unit = {
+
+    Logger.getLogger("org").setLevel(Level.OFF)
+
+    val spark = SparkSession.builder()
+      .appName("Test Application")
+      .master("local[*]")
+      .getOrCreate()
+
+    import spark.implicits._
+
+    val lines: DataFrame = spark.readStream
+        .format("socket")
+        .option("host", "localhost")
+        .option("port", "9999")
+        .load()
+
+    val words = lines.as[String].flatMap(_.split(" "))
+
+    val wordsCount = words.groupBy("value").count()
+
+    val query = wordsCount.writeStream
+        .outputMode("complete")
+        .format("console")
+        .start()
+
+    query.awaitTermination()
+  }
+}
